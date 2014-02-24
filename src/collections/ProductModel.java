b2739b4f7e7d3c7abb5a929f78c577e92b617d7c
sobolev-a <andre.sobolev@gmail.com>
2014-02-25 00:13:46 +0200

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # Explicit paths specified without -i nor -o; assuming --only paths... # On branch master # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	new file:   lib/jsoup-1.7.3.jar #	new file:   out/production/somefunctions/collections/ProductModel.class #	modified:   out/production/somefunctions/pack/Main$Steps.class #	modified:   out/production/somefunctions/pack/Main.class #	modified:   out/production/somefunctions/product/Features$1.class #	modified:   out/production/somefunctions/product/Features$2.class #	modified:   out/production/somefunctions/product/Features.class #	new file:   out/production/somefunctions/product/FeaturesHTML.class #	modified:   out/production/somefunctions/product/MakeObject.class #	modified:   out/production/somefunctions/product/Product.class #	modified:   somefunctions.iml #	new file:   src/collections/ProductModel.java #	modified:   src/pack/Main.java #	new file:   src/product/FeaturesHTML.java # # Changes not staged for commit: #   (use "git add <file>..." to update what will be committed) #   (use "git checkout -- <file>..." to discard changes in working directory) # #	modified:   .idea/misc.xml #	modified:   .idea/workspace.xml # # Untracked files: #   (use "git add <file>..." to include in what will be committed) # #	.idea/libraries/ #	.idea/uiDesigner.xml

---
+++ src/collections/ProductModel.java
@@ -0,0 +1,35 @@
+package collections;
+
+/**
+ * Created by mac on 2/24/14.
+ */
+public class ProductModel {
+
+    private String brand = null;
+    private Double volume = 0.0;
+    private Integer price = 0;
+    private Integer cash = 0;
+
+    public ProductModel(String bBrand, Integer pPrice, Integer cash) {
+        this.brand = bBrand;
+        this.price = pPrice;
+        this.cash = cash;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public Double getVolume() {
+        return this.volume;
+    }
+
+    public Integer getPrice() {
+        return this.price;
+    }
+
+    public Integer getCash() {
+        return this.cash;
+    }
+
+}
---
+++ src/product/FeaturesHTML.java
@@ -0,0 +1,59 @@
+package product;
+
+import collections.ProductModel;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by mac on 2/2/14.
+ */
+public class FeaturesHTML {
+
+    Document doc;
+    Elements product;
+    String productName;
+    ArrayList<ProductModel> products = null;
+
+
+    public void getPageProducts() {
+        products = new ArrayList<>();
+
+        try {
+            int a = 0;
+            do {
+                doc = Jsoup.connect(String.format("https://novus.zakaz.ua/ru/apple-juice/?&page=%s", a)).get();
+
+                product = doc.getElementsByClass("one-product");
+                String test = "as346fvgghfghy8";
+                for (Element link : product) {
+                    productName = link.select(".one-product-name").text();
+                    productName = productName.replace("Сік", "Сок").replace("Напій", "Напиток").replace("Cік", "Сок");
+                    products.add(new ProductModel(productName, Integer.parseInt(link.select(".grivna").text()), Integer.parseInt(link.select(".kopeiki").text().replace("[^0-9]", ""))));
+                    System.out.print("");
+                }
+                a++;
+            } while (a < 3);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showProductList() {
+        int i = 1;
+        Iterator<ProductModel> itr = products.iterator();
+        while (itr.hasNext()) {
+            ProductModel pm = itr.next();
+            System.out.println(String.format("%s. %s %s,%s", i, pm.getBrand(), pm.getPrice(), pm.getCash()));
+            i++;
+        }
+    }
+
+}
